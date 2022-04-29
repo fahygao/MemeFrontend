12391c35940f41850143fa8560b2bd7a93cc28f3
HancyaHan1997 <shelvilia@hotmail.com>
2022-04-29 02:02:26 -0400

    count all chars

--- frontend/src/components/Popup/PopupPost.js
+++ frontend/src/components/Popup/PopupPost.js
@@ -13,24 +13,24 @@ function PopupPost({ setOpenModal }) {
     const wordCount = (event) => {
       // 中文字判断
       let Words = event.srcElement.value;
-      let iTotal = 0;
-      // 数字判断
-      let inum = 0;
-      for (let i = 0; i < Words.length; i++) {
-        let c = Words.charAt(i);
-        //基本汉字
-        if (c.match(/[\u4e00-\u9fa5]/)) {
-          iTotal++;
-        }
-        //基本汉字补充
-        else if (c.match(/[\u9FA6-\u9fcb]/)) {
-          iTotal++;
-        }
-        else if (c.match(/[0-9]/)) {
-          inum++;
-        }
-      }
-      setCount(inum + iTotal);
+      /*  let iTotal = 0;
+        // 数字判断
+        let inum = 0;
+        for (let i = 0; i < Words.length; i++) {
+          let c = Words.charAt(i);
+          //基本汉字
+          if (c.match(/[\u4e00-\u9fa5]/)) {
+            iTotal++;
+          }
+          //基本汉字补充
+          else if (c.match(/[\u9FA6-\u9fcb]/)) {
+            iTotal++;
+          }
+          else if (c.match(/[0-9]/)) {
+            inum++;
+          }
+        } */
+      setCount(Words.length);
     };
 
     window.addEventListener('input', wordCount);
