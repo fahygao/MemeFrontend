4cfb12a79e7357a306405aefaf5627aa58ae5b5d
Eric He <zh975@nyu.edu>
2022-04-30 01:59:24 -0700

    eric change

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,5 +1,5 @@
 export const APP_TITLE = "MeMe";
 
-// export const API_BASE_URL = "http://127.0.0.1:8000"; //local host
-export const API_BASE_URL =
-  "https://cors-everywhere.herokuapp.com/http://44.202.3.146";
+export const API_BASE_URL = "http://127.0.0.1:8000"; //local host
+// export const API_BASE_URL =
+//   "https://cors-everywhere.herokuapp.com/http://44.202.3.146";
