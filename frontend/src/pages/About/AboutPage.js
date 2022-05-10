13933fe9d1ac9285e7c603b64c4e0a50f8f31a5e
Eric He <zh975@nyu.edu>
2022-05-09 22:53:40 -0700

    map 1st commit

--- frontend/src/pages/About/AboutPage.js
+++ frontend/src/pages/About/AboutPage.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { MapContainer, TileLayer, useMap, Marker, Popup } from "react-leaflet";
+import "./AboutPage.css";
 
 function About() {
-  return <div>This is the About page</div>;
+  return (
+    <MapContainer center={[40.7294, -73.9972]} zoom={15} scrollWheelZoom={true}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[51.505, -0.09]}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
 }
 
 export default About;
