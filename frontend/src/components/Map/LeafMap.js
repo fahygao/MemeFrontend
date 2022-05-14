04826fdc935cc42ada50b234d85f49e3e22c3d2f
fahygao <fahygao@gmail.com>
2022-05-13 23:22:55 -0400

    change the map size

--- frontend/src/components/Map/LeafMap.js
+++ frontend/src/components/Map/LeafMap.js
@@ -4,7 +4,7 @@ import "./LeafMap.css";
 
 const LeafMap = (props) => {
   return (
-    <MapContainer center={[40.7294, -73.9972]} zoom={12} scrollWheelZoom={true}>
+    <MapContainer center={[40.7294, -73.9972]} zoom={11} scrollWheelZoom={true}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
