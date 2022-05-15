891374a9d8a28967bbf25c91c013eb7b0a352852
Eric He <zh975@nyu.edu>
2022-05-14 20:47:40 -0700

    map update

--- frontend/src/components/Map/LeafMap.js
+++ frontend/src/components/Map/LeafMap.js
@@ -3,13 +3,28 @@ import { MapContainer, TileLayer, useMap, Marker, Popup } from "react-leaflet";
 import "./LeafMap.css";
 
 const LeafMap = (props) => {
+  const myFilter = ["hue:180deg", "invert:100%"];
+
+  //   let myTileLayer = L.tileLayer
+  //     .colorFilter("https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png", {
+  //       attribution:
+  //         '<a href="https://wikimediafoundation.org/wiki/Maps_Terms_of_Use">Wikimedia</a>',
+  //       filter: myFilter,
+  //     })
+  //     .addTo(map);
+  const not_anom = (name) => {
+    return "@" + name;
+  };
+  const niming = "某人";
   return (
     <MapContainer center={[40.7294, -73.9972]} zoom={11} scrollWheelZoom={true}>
       <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='<a href="https://wikimediafoundation.org/wiki/Maps_Terms_of_Use">Wikimedia</a>'
+        url="https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png"
+        filter={myFilter}
       />
-      {console.log(props.items)}
+
+      {/* {console.log(props.items)} */}
       {props.items.map((story) => {
         // console.log(story);
         if (story.lat != null) {
@@ -19,7 +34,11 @@ const LeafMap = (props) => {
 
           return (
             <Marker id={story} key={story.id} position={[lat, lon]}>
-              <Popup>@{story.username} 在这有故事～</Popup>
+              <Popup>
+                {!story.anonymous && not_anom(story.username)}{" "}
+                {story.anonymous && niming}
+                在这有故事～
+              </Popup>
             </Marker>
           );
         }
