93b105f49342803a25ab479e6cf31a1233ea12df
Eric He <zh975@nyu.edu>
2022-05-16 00:46:09 -0300

    map update

--- frontend/src/components/Map/LeafMap.js
+++ frontend/src/components/Map/LeafMap.js
@@ -4,27 +4,14 @@ import * as L from "leaflet";
 import "./LeafMap.css";
 // import markerIcon from "./../../images/markerIcon.png";
 import customIcon from "./../../images/markerIcon.png";
+import purpleIcon from "./../../images/location_purple.svg";
 
 const LeafMap = (props) => {
-  const LeafIcon = L.Icon.extend({
-    options: { iconSize: [19, 30] },
+  const DefaultIcon = L.icon({
+    iconUrl: purpleIcon,
+    iconSize: new L.Point(20, 25),
   });
-
-  const blueIcon = new LeafIcon({
-      iconUrl:
-        "https://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|abcdef&chf=a,s,ee00FFFF",
-    }),
-    redIcon = new LeafIcon({
-      iconUrl:
-        "https://www.clipartmax.com/png/full/114-1148546_base-marker-gps-location-map-map-marker-marker-icon.png",
-    }),
-    purpleIcon = new LeafIcon({
-      iconUrl:
-        "https://www.clipartmax.com/png/full/342-3426684_location-pointer-location-pointer-purple.png",
-    });
-
-  //  Use the state hook:
-  const [icon, setIcon] = useState(purpleIcon);
+  L.Marker.prototype.options.icon = DefaultIcon;
 
   const not_anom = (name) => {
     return "@" + name;
@@ -34,7 +21,7 @@ const LeafMap = (props) => {
     <MapContainer center={[40.7294, -73.9972]} zoom={11} scrollWheelZoom={true}>
       <TileLayer
         attribution='&copy; <a href="https://stadiamaps.com/">'
-        url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png?api_key=997c57ab-80a0-4272-b23a-bee94473850b"
+        url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png?api_key=997c57ab-80a0-4272-b23a-bee94473850b"
       />
 
       {/* {console.log(props.items)} */}
@@ -46,7 +33,7 @@ const LeafMap = (props) => {
           //   console.log(lat);
 
           return (
-            <Marker id={story} key={story.id} position={[lat, lon]} icon={icon}>
+            <Marker id={story} key={story.id} position={[lat, lon]}>
               <Popup>
                 {!story.anonymous && not_anom(story.username)}{" "}
                 {story.anonymous && niming}
